31381397aca952fc6dadc94ebc820c4ecf4d4f7d
samuelkiruba <kirubasamuel2004@gmail.com>
2025-06-19 17:38:29 +0530

    Week 1 Hands on

---
+++ FactoryMethodPatternExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class PDFDocument : DocumentFactory
+{
+    public override void CreateDocument()
+    {
+        Console.WriteLine("PDF Document Created");
+    }
+}
+class WordDocument : DocumentFactory
+{
+    public override void CreateDocument()
+    {
+        Console.WriteLine("Word Document Created");
+    }
+}
+class ExcelDocument : DocumentFactory
+{
+    public override void CreateDocument()
+    {
+        Console.WriteLine("Excel Document Created");
+    }
+}
+abstract class DocumentFactory
+{
+    public abstract void CreateDocument();
+}
+public class FactoryMethodPatternExample
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory pdf = new PDFDocument();
+        pdf.CreateDocument();
+        DocumentFactory word = new WordDocument();
+        word.CreateDocument();
+        DocumentFactory excel = new ExcelDocument();
+        excel.CreateDocument();
+    }
+}
---
+++ SingletonPatternExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace SingletonPatternExample
+{
+    class Logger
+    {
+        private static Logger _instance;
+        private Logger()
+        {
+            Console.WriteLine("Logger instance created.");
+        }
+        public static Logger getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+    }
+    public class Singleton
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.getInstance();
+        }
+    }
+}
